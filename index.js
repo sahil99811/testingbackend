7e0d356b09ebb20558ba4070e2ddc750ee98a0e7
sahil99811 <sp220253@gmail.com>
2024-04-05 22:48:23 +0530

    third commit

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 app.use(express.json());
-
+app.get("/",(re,res)=>{
+    return res.status(200).json({
+        "name":"sahil patel",
+        "branch":"Information technology",
+        "sem":6
+     
+   })
+})
 app.get('/getBlob',(req,res)=>{
    return res.status(200).json({
         "name":"sahil patel",
